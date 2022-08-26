d24e7f36302fe6d0edc491c4db2d8cf8c43559af
Claudia Troeger <claudiatroeger2410@yahoo.de>
2022-08-26 06:38:27 +0100

    Created seeds and added model articles

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,23 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+puts "Cleaning database ..."
+Article.destroy_all
+Contact.destroy_all
+
+puts "Database clean!"
+puts "Creaing new articles ..."
+
+@articles = [
+  { name: "Article 1", price: 20 },
+  { name: "Article 2", price: 50 },
+  { name: "Article 3", price: 30 },
+]
+
+@articles.each do |article|
+  article = Article.new(article)
+  article.save
+  puts "Created #{article.name}."
+end
+
+puts "Finished!"
